2bd9794f4800845780c30592054432e4d47b0845
anitha12042003 <anithas12042003@gmil.com>
2024-06-25 10:09:24 -0700

    sdf

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -1,3 +1,130 @@
+// // import React, { useState } from "react";
+// // import { Link, useNavigate } from "react-router-dom";
+// // import toast from "react-hot-toast";
+// // import axios from "axios";
+
+// // const Register = () => {
+// //   const navigate = useNavigate();
+// //   const [values, setValues] = useState({
+// //     Username: "",
+// //     Useremail: "",
+// //     Password: "",
+// //     Useraddress:null
+  
+// //   });
+
+// //   const generateError = (err) => toast.error(err, {
+// //     position: "bottom-right",
+// //   });
+
+// //   const handleSubmit = async (e) => {
+// //     e.preventDefault();
+
+// //     try {
+// //       const { data ,token} = await axios.post("http://localhost:2000/api/register",{
+// //         headers:{
+// //           // Authorization:token
+          
+// //         }
+// //       }, {
+// //         ...values,
+// //       }, {
+// //         withCredentials: true,
+// //       });
+
+// //       // console.log(data);
+// //       if (data) {
+// //         if (data.errors) {
+// //           const { Useremail, Password } = data.errors;
+// //           if (Useremail) generateError(Useremail);
+// //           else if (Password) generateError(Password);
+// //         } else {
+// //           navigate("/")
+// //           toast.success("Registration successful!", {
+// //             position: "bottom-right",
+// //           });
+// //         }
+// //       }
+// //     } catch (error) {
+// //       console.log(error);
+// //       generateError("Network error. Please try again later.");
+// //     }
+// //   };
+
+// //   return (
+// //     <>
+// //     <div className="flex justify-center items-center min-h-screen bg-gray-100">
+// //       <div className="h-screen w-screen fixed top-0 left-0 bg-black/50 z-50 backdrop-blur-sm">
+// //         <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-4 shadow-md bg-white dark:bg-gray-900 duration-200 w-[400px] max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl 2xl:max-w-2xl">
+// //           <form className="space-y-6" onSubmit={handleSubmit}>
+// //             <p className="text-center text-primary text-2xl font-medium">
+// //               Register Account
+// //             </p>
+// //             <div>
+// //               <label htmlFor="UserName" className="text-lg">UserName</label>
+// //               <input
+// //                 type="text"
+// //                 name="Username"
+// //                 placeholder="Enter UserName"
+// //                 value={values.Username}
+// //                 onChange={(e) =>
+// //                   setValues({ ...values, [e.target.name]: e.target.value })
+// //                 }
+// //                 className="w-full rounded-lg border-gray-200 p-4 text-sm shadow-sm"
+// //               />
+// //             </div>
+
+// //             <div>
+// //               <label htmlFor="email" className="text-lg">Email</label>
+// //               <input
+// //                 type="email"
+// //                 name="Useremail"
+// //                 placeholder="Enter email"
+// //                 value={values.Useremail}
+// //                 onChange={(e) =>
+// //                   setValues({ ...values, [e.target.name]: e.target.value })
+// //                 }
+// //                 className="w-full rounded-lg border-gray-200 p-4 text-sm shadow-sm"
+// //               />
+// //             </div>
+// //             <div>
+// //               <label htmlFor="password" className="text-lg">
+// //                 Password
+// //               </label>
+// //               <input
+// //                 type="password"
+// //                 name="Password"
+// //                 placeholder="Password"
+// //                 value={values.Password}
+// //                 onChange={(e) =>
+// //                   setValues({ ...values, [e.target.name]: e.target.value })
+// //                 }
+// //                 className="w-full rounded-lg border-gray-200 p-3 text-sm shadow-sm"
+// //               />
+// //             </div>
+
+// //             <button
+// //               type="submit"
+// //               className="block w-full rounded-lg bg-primary px-5 py-3 text-sm font-medium text-white"
+// //             >
+// //               Submit
+// //             </button>
+// //             <span className="block text-center">
+// //               Already have an account?
+// //               <Link to="/login" className="text-primary">
+// //                 Login
+// //               </Link>
+// //             </span>
+// //           </form>
+// //         </div>
+// //       </div>
+// //     </div>
+// //     </>
+// //   );
+// // };
+
+// // export default Register;
+
 
 
 import React, { useState } from "react";
